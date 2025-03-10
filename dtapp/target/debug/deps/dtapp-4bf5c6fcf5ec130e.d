846a016962f88dd290e7109686c1f3dbd0106e1c
Elisabet Clay <157527637+lilibet0@users.noreply.github.com>
2025-03-10 10:27:36 -0400

    Add dependencies for axum and tokio

---
+++ dtapp/target/debug/deps/dtapp-4bf5c6fcf5ec130e.d
@@ -0,0 +1,5 @@
+/Users/lilibet/Documents/Rust/dtag/dtapp/dtapp/target/debug/deps/libdtapp-4bf5c6fcf5ec130e.rmeta: src/main.rs
+
+/Users/lilibet/Documents/Rust/dtag/dtapp/dtapp/target/debug/deps/dtapp-4bf5c6fcf5ec130e.d: src/main.rs
+
+src/main.rs:
---
+++ dtapp/target/debug/deps/dtapp-6ac82053d569846e.d
@@ -0,0 +1,5 @@
+/Users/lilibet/Documents/Rust/dtag/dtapp/dtapp/target/debug/deps/libdtapp-6ac82053d569846e.rmeta: src/main.rs
+
+/Users/lilibet/Documents/Rust/dtag/dtapp/dtapp/target/debug/deps/dtapp-6ac82053d569846e.d: src/main.rs
+
+src/main.rs:
