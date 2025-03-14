69f43070910d40bae3b8dfb8ed6b7cd916e50a85
Elisabet Clay <157527637+lilibet0@users.noreply.github.com>
2025-03-14 12:59:27 -0400

    Create HTML file, /Novice page

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,77 @@
-use axum::{
-    response::Html, routing::get, Router
-};
 use axum::extract::State;
-use sqlx::postgres::PgPoolOptions;
+use axum::{Router, response::Html, routing::get};
 use sqlx::FromRow;
+use sqlx::postgres::PgPoolOptions;
 
 use std::sync::Arc;
 
 #[derive(FromRow, Debug, Clone)]
-pub struct Level {
-    pub name: String,
+pub struct Exercise {
+    pub exercise_name: String,
+}
+
+#[derive(Clone)]
+struct SharedState {
+    pool: sqlx::Pool<sqlx::Postgres>
 }
 
 #[tokio::main]
 async fn main() {
     // Connect to local db
     let pool = PgPoolOptions::new()
-                .max_connections(5)
-                // use your own credentials
-                .connect("postgres://dtapp@localhost/dtappdb")
-                .await
-                .expect("couldn't connect to the database");
+        .max_connections(5)
+        // use your own credentials
+        .connect("postgres://dtapp@localhost/dtappdb")
+        .await
+        .expect("couldn't connect to the database");
 
-    // Fetch levels from db
-    let levels = sqlx::query_as::<_, Level>("select name from obed_level;") 
+        let shared_state = Arc::new(SharedState {pool});
+
+    // Build app
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/novice", get(novicehandler))
+        .with_state(shared_state);
+
+    // listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// Include utf-8 file at **compile** time.
+async fn index() -> Html<&'static str> {
+    Html(std::include_str!("../BlueRibbon.html"))
+}
+
+async fn novicehandler(State(shared_state): State<Arc<SharedState>>) -> Html<String> {
+
+    let exercises = getexercises("Novice", shared_state.pool.clone())
+    .await;
+
+    let mut exercisenames = vec![];
+
+    for name in exercises.iter() {
+        exercisenames.push(name.exercise_name.to_string());
+    }
+        // Read names from table
+        let html = format!(
+            "<h1>Novice Exercises </h1>\n\
+             {:?}", exercisenames
+        );
+    Html(html)
+}
+
+async fn getexercises(level: &str, pool: sqlx::Pool<sqlx::Postgres>) -> Vec<Exercise> {
+    let query = format!(
+        "SELECT exercise_name
+        FROM exercise
+        WHERE class_name = '{}';
+    ", level);
+
+    let exercises = sqlx::query_as::<_, Exercise>(&query)
         .fetch_all(&pool)
         .await
         .unwrap();
 
-        let shared_state = Arc::new(levels);
-
-        // Build app
-        let app = Router::new().route("/", get(startup))
-        .with_state(shared_state);
-
-        // run our app with hyper, listening globally on port 3000
-        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-        axum::serve(listener, app).await.unwrap();
-    
-}
-
-async fn startup(State(state): State<Arc<Vec<Level>>>,) -> Html<String> {
-
-    let mut names = vec![];
-
-    for i in 0..state.len() {
-        names.push(format!("<button> {} </button\n", state[i].name.to_string()));
-    }
-    
-
-        // Read names from table
-        let html = format!(
-            "<h1>This is html! </h1>\n\
-             <h2>HTML is so cool.</h2>\n
-             {:?}", names
-        );
-    // create `Html` type like this
-    Html(html)
+    return exercises;
 }
